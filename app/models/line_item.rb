7fa63dd66b4a867eb953d51ccc5aacdeb8702e5b
TAO LIN <t4ol1n@gmail.com>
2013-01-09 17:11:59 +0800

    create shopping cart display page

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ActiveRecord::Base
   # attr_accessible :cart_id, :product_id
+  attr_accessible :product, :product_id, :id, :update_at, :cart_id, :create_at
   belongs_to :product
   belongs_to :cart
 end

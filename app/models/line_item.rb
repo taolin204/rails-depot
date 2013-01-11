14356b870311e2cfac88b63bae5c12e1e2ec3ec9
unknown <readonly@.(none)>
2013-01-11 17:57:34 +0800

    Create Smart Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,4 +3,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :product, :product_id, :id, :update_at, :cart_id, :create_at
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end

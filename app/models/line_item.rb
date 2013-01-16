bfd597f181499c9f11b0984f29c10b9025d90c99
TAO LIN <t4ol1n@gmail.com>
2013-01-16 15:25:17 +0800

    add order model and order_id for line_item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
   # attr_accessible :cart_id, :product_id
   attr_accessible :product, :product_id, :id, :update_at, :cart_id, :create_at
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
 
